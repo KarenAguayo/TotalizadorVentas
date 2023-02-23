5f51935e219a34be98fac08b96b9878670f80c4f
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-02-23 01:40:25 -0400

    precio total con porcentaje de descuento para 1000 y 3000

--- src/descuento.js
+++ src/descuento.js
@@ -1,9 +1,11 @@
 function descuento(neto) {
     let respuesta;
 
-    if(neto <= 1000)
-    {
+    if(neto <= 1000){
         respuesta = 3;
+    }
+    if((neto > 1000) && (neto <=3000)){
+        respuesta = 5;
     }
 
     return respuesta;
