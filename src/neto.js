33af31a991ee277c0e99ede40fbf2c982eea9b0d
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-02-23 00:59:21 -0400

    mostrar el precio neto

---
+++ src/neto.js
@@ -0,0 +1,8 @@
+function neto(precios, cantidades) {
+    let res;
+    res = precios * cantidades;
+
+    return res;
+  }
+
+  export default neto;
--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,8 @@
 import item from "./item.js";
 import precio from "./precio.js";
 import estado from "./estado.js";
-
-const items = document.querySelector("#cantidad-items");
+import neto from "./neto.js";
+const cantidades = document.querySelector("#cantidad-items");
 const form = document.querySelector("#cantidad-form");
 const div = document.querySelector("#cantidad-div");
 const precios = document.querySelector("#precio-items");
@@ -13,8 +13,8 @@ const form3 = document.querySelector("#estado-form");
 const div3 = document.querySelector("#estado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const ItemsNumber = Number.parseInt(items.value);
-  div.innerHTML = "<p>" + item(ItemsNumber) + "</p>";
+  const CantidadesNumber = Number.parseInt(cantidades.value);
+  div.innerHTML = "<p>" + item(CantidadesNumber) + "</p>";
 });
 form2.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -25,6 +25,9 @@ form3.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const EstadosList = estados.value;
+  const precio = Number.parseInt(precios.value);
+  const cantidad = Number.parseInt(cantidades.value);
   div3.innerHTML = "<p> Estado: " + EstadosList + 
-                   "<p> Impuesto: " + estado(EstadosList) + "</p>";
+                   "<p> Impuesto: " + estado(EstadosList) +
+                   "<p> Precio Neto: " + neto(precio, cantidad) + "</p>";
 });
